e5c0531f44c237905a7085f1821f885f182cf304
bota.lb <bota.lb@alibaba-inc.com>
2020-09-06 17:35:20 +0800

    主题方案成功

--- antd-app/config/webpack.config.js
+++ antd-app/config/webpack.config.js
@@ -86,7 +86,7 @@ module.exports = function(webpackEnv) {
       },
       {
         loader: require.resolve('less-loader'),
-        options: {...cssOptions, javascriptEnabled: true,modifyVars: { 'ant-prefix': 'robin' ,'robin-color':'blue'}},
+        options: {...cssOptions, javascriptEnabled: true,modifyVars: {'robin-color':'blue'}}, // 'ant-prefix': 'robin' ,
       },
       {
         // Options for PostCSS as we reference these options twice
